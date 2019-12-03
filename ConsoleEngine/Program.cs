729e83039b202641edd60018600bbaccf388abc9
Moustafa Khalil <mostafa90@gmail.com>
2019-12-03 23:21:18 +0400

    adding entry projects

---
+++ ConsoleEngine/Program.cs
@@ -0,0 +1,26 @@
+﻿using EngineViewer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Urho3DNet;
+
+namespace ConsoleEngine
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			using (var context = new Context())
+			{
+				using (var application = new DefaultScene(context))
+				{
+					application.Run();
+				}
+			}
+
+		}
+	}
+}
---
+++ WPFEngine/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using EngineViewer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Urho3DNet;
+
+namespace WPFEngine
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow: System.Windows.Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			WindowStyle = WindowStyle.None;
+			Loaded += delegate
+			{
+				 DefaultScene.Parent = rbfxHost.Handle;
+
+				Task.Run(() =>
+				{
+					using (var context = new Context())
+					{
+						using (var application = new DefaultScene(context))
+						{
+							application.Run();
+						}
+					}
+				});
+			};
+		}
+	}
+}
